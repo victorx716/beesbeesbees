80d0dd63260af06faf5e6c4988fc6fc001274060
Victor Liu <victorliu716@gmail.com>
2019-01-09 10:53:53 -0800

    Finished the beesbeesbees solo sprint

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.makeHoney = function(value) {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function(value) {
+  this.honeyPot--;
+}
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
